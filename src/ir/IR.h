67aec5b71da733864891d5724819be6dff6c560b
cerebralstoic <vineet.rawat.ddun62019@gmail.com>
2026-01-17 17:56:15 +0530

    Implemented TAC

---
+++ src/ir/IR.h
@@ -0,0 +1,17 @@
+#ifndef IR_H
+#define IR_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct TAC{
+    string op;
+    string arg1;
+    string arg2;
+    string result;
+};
+
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,22 @@
 #include "lexer/Lexer.h"
 #include "parser/Parser.h"
 #include "semantic/SemanticAnalyzer.h"
+#include "ir/IRGenerator.h"
 #include <iostream>
 
 int main() {
     Lexer lexer("../samples/marks.intent");
     Parser parser(lexer);
-
     ASTNode* ast = parser.parseProgram();
-    std::cout << "Parsing successful." << std::endl;
+
+    std::cout << "Parsing successful.\n";
 
     SemanticAnalyzer sem;
     sem.analyze(ast);
+
+    IRGenerator ir;
+    ir.generate(ast);
+    ir.print();
 
     return 0;
 }
